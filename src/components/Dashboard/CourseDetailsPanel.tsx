7a5e834696d65852e95954c57357668f7bd308fc
nnh1125 <ngan_ha.nguyen@tufts.edu>
2026-02-21 19:36:12 -0500

    setting up CourseDetailsPanel

---
+++ src/components/Dashboard/CourseDetailsPanel.tsx
@@ -0,0 +1,10 @@
+function CourseDetailsPanel() {
+  return (
+    <div>
+      <h2>Course Details</h2>
+      {/* Course details content goes here */}
+    </div>
+  );
+}
+
+export default CourseDetailsPanel;
